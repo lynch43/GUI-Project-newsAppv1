d6d39f35bc4e5aacd6a78877e15056a7126714a2
Danny Lynch <lynch4360@gmail.com>
2024-04-12 21:02:27 +0100

    Fetch the Api and populate the blog container

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-const apikey = 'b20c74e87efa4e89b14c6db7fc70e6b6';
+const apiKey = "b21c74e87efa4e89b14c6db7fc70e6b6";
 
-const blogContainer = document.getElementById('blog-container');
+const blogContainer = document.getElementById("blog-container");
 
-async function fetchRandomNews(){
+async function fetchRandomNews() {
     try{
-        const apiUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=10&apikey=${apikey}`;
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b20c74e87efa4e89b14c6db7fc70e6b6`;
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
+        return data.articles;
     } catch(error){
         console.error("Error fetching random news", error);
         return [];
     }
 }
 
-function displayBlogs(articles){
+function displayBlogs(articles) {
     blogContainer.innerHTML = "";
-    articles.forEach((article)) =>{
-        constblogCard = document.createElement("div");
-        blogCard.classList.add("");
+    articles.forEach((article) =>{
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
         const img = document.createElement("img");
         img.src = article.urlToImage;
         img.alt = article.title;
         const title = document.createElement("h2");
-        title.textContent = article.title;
-        const description = document.createElement("p");
-        description.textContent = article.description;
+        const shortTitle = 
+            article.title.length > 30
+            ? article.title.slice(0, 30) + "..." 
+            : article.title; 
+        title.textContent = shortTitle;
+        const description = document.createElement("p"); 
+        const shortDescription = 
+            article.description.length > 40
+            ? article.description.slice(0, 40) + 
+            "..." 
+            : article.description; 
+        description.textContent = shortDescription;
 
         blogCard.appendChild(img);
         blogCard.appendChild(title);
         blogCard.appendChild(description);
-        blogCard.appendChild(blogCare);
+        blogContainer.appendChild(blogCard);
 
-
-
-
-    }
+    });
 }
 
-async ()=>{
-    try{
+(async () => {
+    try {
         const articles = await fetchRandomNews();
-        displayBlogs(articles)
-        console.log(articles);
-    } catch(error){
+        displayBlogs(articles);
+    } catch (error) {
         console.error("Error fetching random news", error);
- 
+        return [];
     }
-})
+})();
